a2c637a145a525809c78e4e96d76c5a55d7579b5
NALLAMILLILAKSHMIMVIGNESWARAREDDY <98519503+NALLAMILLILAKSHMIMVIGNESWARAREDDY@users.noreply.github.com>
2022-07-01 16:08:42 +0530

    Create 10exp.py

---
+++ 10exp.py
@@ -0,0 +1,7 @@
+'''Write a program that asks the user for a large integer and inserts commas into it 
+according to the standard American convention for commas in large numbers. For 
+instance, if the user enters 1000000, the output should be 1,000,000.'''
+n=int(input("Enter a number:"))
+
+print("The number seperated with commas is:{:,}".format(n))
+
